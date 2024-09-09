52cdfbd033923e46ae0fe90ba5dbea62db57eb3d
MauricioNLM <Maurcio.Navarro@sdgku.edu>
2024-09-09 12:44:53 -0700

    Assignment2

--- script/register.js
+++ script/register.js
@@ -1,24 +1,44 @@
+
+class Pet {
+    age;
+    gender;
+    breed;
+    service;
+    constructor(name, type = 'Basic') {
+        this.name = name;
+        this.type = type;
+    }
+}
+
+
 console.log("Helo Word")
 
 let pets = [];
 
 
-let cat = {
-    name:"Michi",
-    age:"5",
-    gender:"M",
-    breed:"siames"
-} 
+// let cat = {
+//     name:"Michi",
+//     age:"5",
+//     gender:"M",
+//     breed:"siames"
+// } 
 
-let dog = {
-    name:"Black",
-    age:"8",
-    gender:"M",
-    breed:"pastor "
-} 
+// let dog = {
+//     name:"Black",
+//     age:"8",
+//     gender:"M",
+//     breed:"pastor "
+// } 
 
-document.write(cat.name)
-document.write(dog.name)
+// document.write(cat.name)
+// document.write(dog.name)
+
+// pets.push(cat,dog)
+
+let cat = new Pet('michi');
+
+
+let dog = new Pet('black');
 
 pets.push(cat,dog)
 
@@ -30,4 +50,26 @@ function displayPets(array)
     }
 }
 
+
+
+
+
 displayPets(pets)
+
+function Restister(){
+    let name = document.getElementById('namePet').value
+    let pet = new Pet(name);
+
+    pet.age = document.getElementById('agePet').value
+    pet.gender = document.getElementById('genderPet').value
+    pet.breed = document.getElementById('breedPet').value
+    pet.service = document.getElementById('servicePet').value
+    pet.type = document.getElementById('typeSer').value
+
+
+    pets.push(pet)
+
+    
+
+    displayPets(pets)
+}
